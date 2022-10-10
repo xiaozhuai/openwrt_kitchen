d08b3ae8a85be850e97e5a11541b9509117c1c60
xiaozhuai, Weihang Ding <798047000@qq.com>
2022-10-10 12:33:29 +0800

    Update 023_themes.sh

--- kitchen/scripts.d/023_themes.sh
+++ kitchen/scripts.d/023_themes.sh
@@ -1,6 +1,3 @@
-echo "  - Install luci-compat"
-opkg install luci-compat
-
 install_by_cond "${INSTALL_LUCI_THEME_BOOTSTRAP}" luci-theme-bootstrap
 install_by_cond "${INSTALL_LUCI_THEME_MATERIAL}" luci-theme-material
 download_and_install_by_cond "${INSTALL_LUCI_THEME_ARGON}" "https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9.4/luci-theme-argon-master_2.2.9.4_all.ipk"
