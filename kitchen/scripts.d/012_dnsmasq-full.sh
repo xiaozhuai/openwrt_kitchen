4f5b3ea375f13cb77d386459f96409b4ea9f65e3
xiaozhuai <798047000@qq.com>
2023-01-08 18:40:25 +0800

    Fix resolve feeds ip

--- kitchen/scripts.d/012_dnsmasq-full.sh
+++ kitchen/scripts.d/012_dnsmasq-full.sh
@@ -1,7 +1,11 @@
 if [ "${USE_DNSMASQ_FULL}" = "true" ]; then
   cp -f /etc/hosts /etc/hosts.bak
-  feeds_ip="$(nslookup downloads.openwrt.org | tail -n +3 | sed -n 's/Address:\s*//p' | grep -v -e : | head -n 1)"
+  feeds_ip="$(nslookup downloads.openwrt.org | tail -n +3 | sed -n 's/Address\s*\d*:\s*//p' | grep -v -e : | head -n 1)"
   echo "${feeds_ip} downloads.openwrt.org" >>/etc/hosts
+  if [ -z "${feeds_ip}" ]; then
+    echo "Cannot Resolve downloads.openwrt.org"
+    exit 1
+  fi
   echo "  - Resolve downloads.openwrt.org ${feeds_ip}"
 
   echo "  - Remove dnsmasq"
