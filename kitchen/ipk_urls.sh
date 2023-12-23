d484aa1fb2325e76e95e099b8ca5460b99f965a9
Ray Hong <rayyounghong@gmail.com>
2023-12-23 14:43:37 +0800

    升级 openwrt 及插件版本 (#1)

--- kitchen/ipk_urls.sh
+++ kitchen/ipk_urls.sh
@@ -1,8 +1,8 @@
-export LUCI_OPENCLASH_IPK_FILE="luci-app-openclash_0.45.78-beta_all.ipk"
-export LUCI_OPENCLASH_IPK_URL="https://github.com/vernesong/OpenClash/releases/download/v0.45.78-beta/luci-app-openclash_0.45.78-beta_all.ipk"
+export LUCI_OPENCLASH_IPK_FILE="luci-app-openclash_0.45.157-beta_all.ipk"
+export LUCI_OPENCLASH_IPK_URL="https://github.com/vernesong/OpenClash/releases/download/v0.45.157-beta/luci-app-openclash_0.45.157-beta_all.ipk"
 
 export LUCI_ALIDDNS_IPK_FILE="luci-app-aliddns_0.4.0-1_all.ipk"
 export LUCI_ALIDDNS_IPK_URL="https://github.com/honwen/luci-app-aliddns/releases/download/v20210117/luci-app-aliddns_0.4.0-1_all.ipk"
 
-export LUCI_THEME_ARGON_IPX_FILE="luci-theme-argon-master_2.2.9.4_all.ipk"
-export LUCI_THEME_ARGON_IPX_URL="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.9.4/luci-theme-argon-master_2.2.9.4_all.ipk"
+export LUCI_THEME_ARGON_IPX_FILE="luci-theme-argon_2.3.1_all.ipk"
+export LUCI_THEME_ARGON_IPX_URL="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.1/luci-theme-argon_2.3.1_all.ipk"
