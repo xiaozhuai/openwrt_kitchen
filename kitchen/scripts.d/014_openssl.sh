dd44eb22d43f5081421fec1edb5c9d0481d8afd8
xiaozhuai <798047000@qq.com>
2024-01-16 14:42:08 +0800

    Fix use openssl on openwrt 23.x

--- kitchen/scripts.d/014_openssl.sh
+++ kitchen/scripts.d/014_openssl.sh
@@ -1,6 +1,10 @@
 if [ "${USE_OPENSSL}" = "true" ]; then
+  echo "  - Remove luci-ssl"
+  opkg remove luci-ssl
   echo "  - Remove wolfssl"
-  opkg remove luci-ssl libustream-wolfssl* px5g-wolfssl libwolfssl*
-  echo "  - Install openssl"
+  opkg remove libustream-wolfssl* px5g-wolfssl libwolfssl*
+  echo "  - Remove mbedtls"
+  opkg remove libustream-mbedtls* px5g-mbedtls libmbedtls*
+  echo "  - Install luci-ssl-openssl"
   opkg install luci-ssl-openssl
 fi
