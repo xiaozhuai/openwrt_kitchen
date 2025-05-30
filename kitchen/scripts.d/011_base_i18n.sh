3dba010c3a045a617863552fd17626b9f9688267
Weihang Ding <798047000@qq.com>
2025-05-30 17:35:08 +0800

    Update 011_base_i18n.sh

--- kitchen/scripts.d/011_base_i18n.sh
+++ kitchen/scripts.d/011_base_i18n.sh
@@ -2,8 +2,8 @@ if [ -n "${LUCI_LANGUAGE}" ]; then
   version_id=$(. /etc/os-release && echo "$VERSION_ID")
   major_version=$(echo "$version_id" | cut -d '.' -f1)
   if [ "$major_version" -ge 24 ]; then
-    echo "  - Install luci-i18n-base-${LUCI_LANGUAGE} luci-i18n-firewall-${LUCI_LANGUAGE}"
-    opkg install "luci-i18n-base-${LUCI_LANGUAGE}" "luci-i18n-firewall-${LUCI_LANGUAGE}"
+    echo "  - Install luci-i18n-base-${LUCI_LANGUAGE} luci-i18n-package-manager-${LUCI_LANGUAGE} luci-i18n-firewall-${LUCI_LANGUAGE}"
+    opkg install "luci-i18n-base-${LUCI_LANGUAGE}" "luci-i18n-package-manager-${LUCI_LANGUAGE}" "luci-i18n-firewall-${LUCI_LANGUAGE}"
   else
     echo "  - Install luci-i18n-base-${LUCI_LANGUAGE} luci-i18n-opkg-${LUCI_LANGUAGE} luci-i18n-firewall-${LUCI_LANGUAGE}"
     opkg install "luci-i18n-base-${LUCI_LANGUAGE}" "luci-i18n-opkg-${LUCI_LANGUAGE}" "luci-i18n-firewall-${LUCI_LANGUAGE}"
