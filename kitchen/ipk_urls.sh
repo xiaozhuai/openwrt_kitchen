39efcc9cb1b14f367743c5506139ab4318e8c76b
xiaozhuai <798047000@qq.com>
2025-10-12 00:38:51 +0800

    Update base version & ipks

--- kitchen/ipk_urls.sh
+++ kitchen/ipk_urls.sh
@@ -1,8 +1,8 @@
-export LUCI_OPENCLASH_IPK_FILE="luci-app-openclash_0.46.079_all.ipk"
-export LUCI_OPENCLASH_IPK_URL="https://github.com/vernesong/OpenClash/releases/download/v0.46.079/luci-app-openclash_0.46.079_all.ipk"
+export LUCI_OPENCLASH_IPK_FILE="luci-app-openclash_0.47.015_all.ipk"
+export LUCI_OPENCLASH_IPK_URL="https://github.com/vernesong/OpenClash/releases/download/v0.47.015/luci-app-openclash_0.47.015_all.ipk"
 
 export LUCI_ALIDDNS_IPK_FILE="luci-app-aliddns_0.4.0-1_all.ipk"
 export LUCI_ALIDDNS_IPK_URL="https://github.com/honwen/luci-app-aliddns/releases/download/v20210117/luci-app-aliddns_0.4.0-1_all.ipk"
 
-export LUCI_THEME_ARGON_IPX_FILE="luci-theme-argon_2.3.1_all.ipk"
-export LUCI_THEME_ARGON_IPX_URL="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.3.1/luci-theme-argon_2.3.1_all.ipk"
+export LUCI_THEME_ARGON_IPX_FILE="luci-theme-argon-2.4.3-r20250722.apk"
+export LUCI_THEME_ARGON_IPX_URL="https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.4.3/luci-theme-argon-2.4.3-r20250722.apk"
